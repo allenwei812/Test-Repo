173dfcc20ba6b4efd04ccf926512c7864c6e39f5
AZ <34956316@users.noreply.github.com>
2021-02-12 10:21:04 +0800

    Initial Commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+from os import rename
+import math
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello,{}".format(who_to_greet)
+    return greeting
+
+
+print(greet("World"))
+print(greet("Allen"))
+r = requests.get("https://coreyms.com")
+print(r.status_code)
